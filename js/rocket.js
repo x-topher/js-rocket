d62a883259c719f329b6dd74eaa511a27fd5ea67
x-topher <54594582+x-topher@users.noreply.github.com>
2021-06-28 14:38:05 -0400

    js functionality added

---
+++ js/rocket.js
@@ -0,0 +1,38 @@
+var timer = null;
+var countDownNumber = 3;
+
+var changeState = function (state) {
+
+	document.body.className = 'body-state'+state;
+	
+	clearInterval(timer);
+
+	countDownNumber = 3;
+	document.getElementById('countdown').innerHTML = countDownNumber;
+
+
+
+	if (state == 2) {
+
+		timer = setInterval (function () {
+		document.getElementById('countdown').innerHTML = countDownNumber - 1;
+		countDownNumber = countDownNumber - 1;
+
+		if (countDownNumber <= 0) {
+			changeState(3);
+		};
+		}, 1000);
+
+	} else if (state == 3) {
+		var success = setTimeout(function() {
+			var randdomNumber = Math.round(Math.random()*10);
+			console.log('randdomNumber: ', randdomNumber);
+			if (randdomNumber > 5) {
+				changeState(4);
+			} else {
+				changeState(5);
+			}
+
+		}, 2000);
+	}
+};
--- js/script.js
+++ js/script.js
@@ -1,3 +1,12 @@
+var doCoolStuff = function () {
+    var currentClassName = document.getElementById('cool').className;
+    if (currentClassName == 'cool') {
+        document.getElementById('cool').className = ('cool red');
+    } else {
+        document.getElementById('cool').className = ('cool');
+    }
+}
+
 var sayMyName = function (name) {
     alert('My name is ' + name);
 }
